aba04b6433146e8b503b269c1cb29e6c57841548
Manu Ponce <kmanuponce@gmail.com>
2021-04-30 14:25:01 -0700

    fulfilled steps 1-2

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    # db config
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    # Import models here
+    # from app.models.book import Book      #make Planet
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register Blueprints here
+    # from .routes import hello_world_bp
+    # app.register_blueprint(hello_world_bp)
+    
     return app
